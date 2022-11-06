50e67725580ba1a421887a139586ff30dd80ee52
Pawel Kazanecki <pawel.kazanecki@capgemini.com>
2022-11-06 21:38:07 +0100

    PrimaryInjectedGreetingService with Primary annotation added

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -2,11 +2,17 @@ package guru.springframework.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.sfgdi.services.PrimaryInjectedGreetingService;
+
 @Controller
 public class MyController {
+    private final PrimaryInjectedGreetingService primaryInjectedGreetingService;
+
+    public MyController(PrimaryInjectedGreetingService primaryInjectedGreetingService) {
+        this.primaryInjectedGreetingService = primaryInjectedGreetingService;
+    }
 
     public String sayHello() {
-        System.out.println("Hello world!!!");
-        return "Hi Folks!";
+        return primaryInjectedGreetingService.sayGreeting();
     }
 }
