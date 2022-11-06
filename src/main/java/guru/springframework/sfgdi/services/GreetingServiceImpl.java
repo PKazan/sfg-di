79efecb77f8e8ae0b328125254157246d3541de0
Pawel Kazanecki <pawel.kazanecki@capgemini.com>
2022-11-06 20:49:41 +0100

    Injection via Property, Setter and  Constructor added

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgdi.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    public String sayGreeting() {
+        return "Hello World";
+    }
+}
