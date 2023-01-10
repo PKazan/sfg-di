58036498130cf109da75d84fa8bfe627da5a42f1
Pawel Kazanecki <pawel.kazanecki@capgemini.com>
2023-01-10 22:53:21 +0100

    @Service removed from Services with @Primary and @Profile, added configuration of this services in config class - continuation of new approach

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,13 +2,36 @@ package guru.springframework.sfgdi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
+import guru.springframework.sfgdi.services.I18nEnglishService;
+import guru.springframework.sfgdi.services.I18nSpanishService;
+import guru.springframework.sfgdi.services.PrimaryInjectedGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingServiceImpl;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishService i18nSpanishService() {
+        return new I18nSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishService i18nService() {
+        return new I18nEnglishService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryInjectedGreetingService primaryInjectedGreetingService() {
+        return new PrimaryInjectedGreetingService();
+    }
 
     @Bean
     ConstructorInjectedGreetingService constructorInjectedGreetingService() {
--- src/main/java/guru/springframework/sfgdi/services/PrimaryInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryInjectedGreetingService.java
@@ -1,10 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-@Primary
-@Service
 public class PrimaryInjectedGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
