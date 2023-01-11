d9b7cf162879da7b644da2f613d0ad321a691ac9
Pawel Kazanecki <pawel.kazanecki@capgemini.com>
2023-01-11 22:31:51 +0100

    factory beans example added

--- src/main/java/com/springframework/pets/DogPetService.java
+++ src/main/java/com/springframework/pets/DogPetService.java
@@ -1,13 +1,8 @@
 package com.springframework.pets;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Profile({"dog", "default"})
-public class DogPetService implements PetService{
+public class DogPetService implements PetService {
     @Override
     public String getPetType() {
-        return  "Dogs are the best!";
+        return "Dogs are the best!";
     }
 }
